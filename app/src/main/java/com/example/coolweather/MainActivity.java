8eba86f00fda032d8341c47db38b80a639829bca
e <yaoyuan1223@foxmail.com>
2018-06-13 23:09:04 +0800

    test update

--- app/src/main/java/com/example/coolweather/MainActivity.java
+++ app/src/main/java/com/example/coolweather/MainActivity.java
@@ -18,8 +18,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Toast.makeText(getApplicationContext(),"刘静文  最漂亮！！！",Toast.LENGTH_SHORT).show();
-                Log.i("xoxo"+ "123");
-                Log.i("xoxo"+ "123");  
+
             }
         });
     }
