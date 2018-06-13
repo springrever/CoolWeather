a932ecc8c7f6b7ef9715f422902a037180797c0e
e <yaoyuan1223@foxmail.com>
2018-06-13 22:55:18 +0800

    try update

--- app/src/main/java/com/example/coolweather/MainActivity.java
+++ app/src/main/java/com/example/coolweather/MainActivity.java
@@ -2,12 +2,23 @@ package com.example.coolweather;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textView = findViewById(R.id.text);
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(),"刘静文  最漂亮！！！",Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
